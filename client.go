0cf91c17c08dd6da3eeb3bdea1a42fb98604a0ba
Frederick Rice <frederick.rice@cloudera.com>
2016-08-02 11:58:21 -0700

    ready for version 1 release

--- client.go
+++ client.go
@@ -1,7 +1,15 @@
+// sasl is a wrapper for the cyrus sasl library written for go.
+// Right now it only supports clients, but that could change in the future.
+// It is meant as a simple interface for interacting with a multitude of
+// authentication mechanisms. Use it just as you would the C library --
+// initialize a Client, call the client's Start method, then keep calling
+// Step until the authentication has completed. After Client is done, call
+// its Free method to cleanup any extra memory resources. The library was
+// written such that multiple calls to Free is ok.
 package sasl
 
-// #cgo LDFLAGS: -lsasl2 -g -O0
-// #cgo CFLAGS: -g -O0
+// #cgo LDFLAGS: -lsasl2
+// #cgo CFLAGS: -Wall
 // #include <sasl/sasl.h>
 // #include <stdio.h>
 // #include <stdlib.h>
